06ff0691e42953a13f973e0d0c75595696a7c89e
GaMer0331 <100282530+GaMer0331@users.noreply.github.com>
2026-02-27 23:14:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// 1. Supabase ma'lumotlarini o'zgaruvchiga olamiz
+const supabaseUrl = 'https://xhoopiedolojpdkhypfn.supabase.co';
+const supabaseKey = 'sb_publishable_3huAanMDccDDQ_2IL6PlNw_zlVYGeQx';
+
+// 2. Mijozni yaratamiz (window.supabase orqali chaqiramiz)
+const _supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+// 3. Ulanishni tekshirish uchun kichik test
+async function testConnection() {
+    try {
+        const { data, error } = await _supabase.from('posts').select('*');
+        
+        if (error) {
+            console.error("Ulanishda xato bor:", error.message);
+        } else {
+            console.log("G'alaba! Bazadan ma'lumot keldi:", data);
+        }
+    } catch (err) {
+        console.error("Kutilmagan xato:", err);
+    }
+}
+
+async function displayPosts() {
+    const { data: posts, error } = await _supabase.from('posts').select('*');
+
+    if (error) {
+        console.error("Xato:", error.message);
+        return;
+    }
+
+    const container = document.getElementById('post-container');
+    container.innerHTML = ''; // Oldingi matnlarni tozalash
+
+    posts.forEach(post => {
+        // Har bir maqola uchun HTML yasaymiz
+        const postHTML = `
+            <article class="post-card">
+                <img src="${post.image_url}" alt="${post.title}">
+                <h1>${post.title}</h1>
+                <p>${post.content}</p>
+            </article>
+        `;
+        container.innerHTML += postHTML; // Sahifaga qo'shish
+    });
+}
+
+testConnection();
+displayPosts();
